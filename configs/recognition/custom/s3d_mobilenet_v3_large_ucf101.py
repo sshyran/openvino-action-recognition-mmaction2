c798dbeba055ce5749eb7ddbf9e8724afb07d16a
Evgeny Izutov <evgeny.izutov@intel.com>
2020-11-24 11:59:15 +0300

    Enabled MixUp

--- configs/recognition/custom/s3d_mobilenet_v3_large_ucf101.py
+++ configs/recognition/custom/s3d_mobilenet_v3_large_ucf101.py
@@ -142,7 +142,7 @@ train_pipeline = [
          contrast_range=(0.6, 1.4),
          saturation_range=(0.7, 1.3),
          hue_delta=18),
-    # dict(type='MixUp',  annot='imagenet_train_list.txt', imgs_root='imagenet/train', alpha=0.2),
+    dict(type='MixUp',  annot='imagenet_train_list.txt', imgs_root='imagenet/train', alpha=0.2),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='FormatShape', input_format='NCTHW'),
     dict(type='Collect', keys=['imgs', 'label', 'dataset_id'], meta_keys=[]),
