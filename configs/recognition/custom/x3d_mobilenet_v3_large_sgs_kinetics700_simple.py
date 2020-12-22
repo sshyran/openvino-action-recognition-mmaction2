49e3e4eb4aa3723fd205832395a08798c973e1f0
Evgeny Izutov <evgeny.izutov@intel.com>
2020-12-22 14:14:19 +0300

    Added config for Kinetics-700

--- configs/recognition/custom/x3d_mobilenet_v3_large_sgs_kinetics700_simple.py
+++ configs/recognition/custom/x3d_mobilenet_v3_large_sgs_kinetics700_simple.py
@@ -1,5 +1,5 @@
 # global parameters
-num_videos_per_gpu = 14
+num_videos_per_gpu = 12
 num_workers_per_gpu = 3
 train_sources = 'kinetics700',
 test_sources = 'kinetics700',
@@ -13,6 +13,8 @@ reset_layer_suffixes = None
 
 # model settings
 input_img_size = 224
+clip_len = 16
+frame_interval = 2
 
 model = dict(
     type='Recognizer3D',
@@ -24,32 +26,19 @@ model = dict(
         pretrained2d=False,
         width_mult=1.0,
         pool1_stride_t=1,
-        # block ids:      0  1  2  3  4  5  6  7  8  9  10 11 12 13 14
-        temporal_strides=(1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1),
-        temporal_kernels=(5, 3, 3, 3, 3, 5, 5, 3, 3, 5, 3, 3, 3, 3, 3),
-        # use_st_att=      (0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0),
-        use_dw_temporal=0,
-        attention_cfg=dict(
-            kernels=3,
-            add_temporal=False,
-            gumbel=True,
-            enable_loss=True,
-            gt_regression=True,
-            tv_loss=True,
-            reg_weight=0.1,
-        ),
+        # block ids       0  1  2  3  4  5  6  7  8  9  10 11 12 13 14
+        # spatial strides 1  2  1  2  1  1  2  1  1  1  1  1  1  2  1
+        temporal_strides=(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1),
+        temporal_kernels=(1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3),
+        use_dw_temporal= (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
         use_temporal_avg_pool=True,
-        input_bn=False,
         out_conv=True,
-        out_attention=False,
-        weight_norm='none',
-        center_conv_weight=None,
-        dropout_cfg=dict(
-            dist='gaussian',
-            p=0.1,
-            mu=0.1,
-            sigma=0.03,
-        ),
+        sgs_cfg=dict(
+            idx=[1],
+            bins=[8],
+            internal_factor=3.0,
+            embd_size=16
+        )
     ),
     reducer=dict(
         type='AggregatorSpatialTemporalModule',
@@ -61,50 +50,15 @@ model = dict(
     ),
     cls_head=dict(
         type='ClsHead',
-        num_classes=700,
+        num_classes=101,
         temporal_size=1,
         spatial_size=1,
         dropout_ratio=None,
         in_channels=960,
-        embedding=True,
-        embd_size=256,
-        num_centers=1,
-        st_scale=10.0,
-        reg_weight=1.0,
-        reg_threshold=0.1,
-        enable_sampling=False,
-        adaptive_sampling=False,
-        sampling_angle_std=3.14 / 2 / 5,
-        enable_class_mixing=False,
-        class_mixing_alpha=0.2,
+        embedding=False,
         loss_cls=dict(
-            type='AMSoftmaxLoss',
-            target_loss='ce',
-            scale_cfg=dict(
-                type='PolyScalarScheduler',
-                start_scale=30.0,
-                end_scale=5.0,
-                power=1.2,
-                num_epochs=41.276,
-            ),
-            pr_product=False,
-            margin_type='cos',
-            margin=0.35,
-            gamma=0.0,
-            t=1.0,
-            conf_penalty_weight=0.085,
-            filter_type='positives',
-            top_k=None,
-            enable_class_weighting=False,
-            enable_adaptive_margins=False,
-        ),
-        losses_extra=dict(
-            loss_lpush=dict(
-                type='LocalPushLoss',
-                margin=0.1,
-                weight=1.0,
-                smart_margin=True,
-            ),
+            type='CrossEntropyLoss',
+            loss_weight=1.0
         ),
     ),
 )
@@ -126,7 +80,11 @@ img_norm_cfg = dict(
 )
 train_pipeline = [
     dict(type='DecordInit'),
-    dict(type='SampleFrames', clip_len=16, frame_interval=2, num_clips=1, temporal_jitter=True),
+    dict(type='SampleFrames',
+         clip_len=clip_len,
+         frame_interval=frame_interval,
+         num_clips=1,
+         temporal_jitter=True),
     dict(type='DecordDecode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='RandomRotate', delta=10, prob=0.5),
@@ -137,7 +95,6 @@ train_pipeline = [
          max_wh_scale_gap=1),
     dict(type='Resize', scale=(input_img_size, input_img_size), keep_ratio=False),
     dict(type='Flip', flip_ratio=0.5),
-    dict(type='BlockDropout', scale=0.2, prob=0.1),
     dict(type='PhotometricDistortion',
          brightness_range=(65, 190),
          contrast_range=(0.6, 1.4),
@@ -150,7 +107,7 @@ train_pipeline = [
 ]
 val_pipeline = [
     dict(type='DecordInit'),
-    dict(type='SampleFrames', clip_len=16, frame_interval=2, num_clips=1, test_mode=True),
+    dict(type='SampleFrames', clip_len=clip_len, frame_interval=frame_interval, num_clips=1, test_mode=True),
     dict(type='DecordDecode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='CenterCrop', crop_size=(input_img_size, input_img_size)),
@@ -176,12 +133,12 @@ data = dict(
     ),
     val=dict(
         source=test_sources,
-        ann_file='val.txt',
+        ann_file='test.txt',
         pipeline=val_pipeline
     ),
     test=dict(
         source=test_sources,
-        ann_file='val.txt',
+        ann_file='test.txt',
         pipeline=val_pipeline
     )
 )
@@ -203,22 +160,20 @@ optimizer_config = dict(
 # parameter manager
 params_config = dict(
     type='FreezeLayers',
-    epochs=5,
+    epochs=0,
     open_layers=['cls_head']
 )
 
 # learning policy
 lr_config = dict(
     policy='customstep',
-    step=[30, 50],
-    fixed_epochs=5,
-    fixed_ratio=1.0,
+    step=[50, 80],
     gamma=0.1,
-    warmup='linear',
-    warmup_epochs=5,
-    warmup_ratio=1e-2,
+    warmup='cos',
+    warmup_epochs=10,
+    warmup_ratio=1e-3,
 )
-total_epochs = 65
+total_epochs = 110
 
 # workflow
 workflow = [('train', 1)]
@@ -226,14 +181,14 @@ checkpoint_config = dict(
     interval=1
 )
 evaluation = dict(
-    interval=1,
+    interval=10,
     metrics=['top_k_accuracy', 'mean_class_accuracy', 'ranking_mean_average_precision'],
     topk=(1, 5),
 )
 
 log_level = 'INFO'
 log_config = dict(
-    interval=10,
+    interval=100,
     hooks=[
         dict(type='TextLoggerHook'),
         dict(type='TensorboardLoggerHook'),
