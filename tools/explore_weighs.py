e9d798d7120a7e0038199780f011d64e2338fa2f
Evgeny Izutov <evgeny.izutov@intel.com>
2021-03-02 11:19:12 +0300

    Minor

--- tools/explore_weighs.py
+++ tools/explore_weighs.py
@@ -79,6 +79,25 @@ def collect_conv_layers(model, eps=1e-5):
             last_conv['bias'] = scales * (last_conv['bias'] - running_mean) + beta
             last_conv['updated'] = True
 
+            # num_filters = last_conv['weight'].shape[0]
+            # filters = np.copy(last_conv['weight']).reshape([num_filters, -1])
+            # norms = np.sqrt(np.sum(np.square(filters), axis=-1))
+            # max_ratio = np.max(norms) / np.median(norms)
+            # filters_ratio = np.median(norms) / norms
+            # threshold_ratio = np.percentile(filters_ratio, 95)
+            # invalid_mask = filters_ratio > threshold_ratio
+            # num_invalid = np.sum(invalid_mask)
+            # if num_invalid > 1:
+            #     conv_name = last_conv['name']
+            #     print(f'Invalid: {conv_name} ({num_invalid} filters) - Max/median: {max_ratio}')
+            #     print(f'   * running_mean: {running_mean[invalid_mask]}')
+            #     print(f'   * running_var: {running_var[invalid_mask]}')
+            #     print(f'   * norm: {norms[invalid_mask]}')
+            #     print(f'   * scale: {scales.reshape(-1)[invalid_mask]}')
+            #     print(f'   * ratio: {filters_ratio[invalid_mask]}')
+            #
+            #     exit()
+
     return conv_layers
 
 
@@ -86,6 +105,7 @@ def show_stat(conv_layers, max_scale=10.0, max_similarity=0.8, sim_percentile=95
     invalid_weight_scales = []
     invalid_bias_scales = []
     invalid_sim = []
+    total_num_invalid_filters, total_num_filters = 0, 0
     for conv in conv_layers:
         name = conv['name']
         weights = conv['weight']
@@ -95,12 +115,12 @@ def show_stat(conv_layers, max_scale=10.0, max_similarity=0.8, sim_percentile=95
             kernel_type += ', fused'
 
         num_filters = weights.shape[0]
+        total_num_filters += num_filters
         filters = weights.reshape([num_filters, -1])
 
         norms = np.sqrt(np.sum(np.square(filters), axis=-1))
         min_norm, max_norm = np.min(norms), np.max(norms)
         median_norm = np.median(norms)
-        scale = max_norm / min_norm
 
         if num_filters <= filters.shape[1]:
             norm_filters = filters / norms.reshape([-1, 1])
@@ -118,9 +138,11 @@ def show_stat(conv_layers, max_scale=10.0, max_similarity=0.8, sim_percentile=95
                 invalid_sim.append((name, kernel_type, sim, num_invalid, num_filters))
 
         scales = max_norm / norms
-        num_invalid = np.sum(scales > max_scale)
-        if num_invalid > 0 or median_norm < 0.1:
-            invalid_weight_scales.append((name, kernel_type, min_norm, median_norm, scale, num_invalid, num_filters))
+        threshold_scale = np.percentile(scales, 95)
+        num_invalid = np.sum(scales > max(threshold_scale, max_scale))
+        if num_invalid > 0:
+            invalid_weight_scales.append((name, kernel_type, min_norm, median_norm, threshold_scale, num_invalid, num_filters))
+            total_num_invalid_filters += num_invalid
 
         bias_scores = np.abs(bias)
         bias_score = np.percentile(bias_scores, 95)
@@ -128,11 +150,12 @@ def show_stat(conv_layers, max_scale=10.0, max_similarity=0.8, sim_percentile=95
             invalid_bias_scales.append((name, kernel_type, bias_score))
 
     if len(invalid_weight_scales) > 0:
-        print('\nFound {} layers with invalid weight norm fraction (max/cur > {}):'
-              .format(len(invalid_weight_scales), max_scale))
+        print(f'\nFound {len(invalid_weight_scales)} layers with invalid weight norm '
+              f'of filters ({total_num_invalid_filters} / {total_num_filters} filters):')
         for name, kernel_type, min_norm, median_norm, scale, num_invalid, num_filters in invalid_weight_scales:
-            print('   - {} ({}): {:.3f} (min={:.3f} median={:.3f} invalid: {} / {})'
-                  .format(name, kernel_type, scale, min_norm, median_norm, num_invalid, num_filters))
+            print(f'   - {name} ({kernel_type}): {scale:.3f} '
+                  f'(min={min_norm:.3f} median={median_norm:.3f} '
+                  f'invalid: {num_invalid} / {num_filters})')
     else:
         print('\nThere are no layers with invalid weight norm.')
 
